a8bd76eb9bb89ba27024dcdd89492318f6f9fe61
Kramskiy Dima <64987701+ifinddima@users.noreply.github.com>
2020-10-14 20:47:02 +0300

    выполняет задания в 4 модуле

---
+++ js/task-04.js
@@ -0,0 +1,31 @@
+const account = {
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  showOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    this.orders.push(order);
+  },
+};
+const copyAccount = Object.assign({}, account);
+copyAccount.orders = [...account.orders];
+// копируем для автотестов ссылочные типы
+
+copyAccount.changeDiscount(0.15);
+console.log(copyAccount.discount); // 0.15
+
+console.log(copyAccount.showOrders());
+// ['order-1', 'order-2', 'order-3']
+
+copyAccount.addOrder(5000, 'order-4');
+console.log(copyAccount.balance); // 19000
+
+console.log(copyAccount.showOrders());
+// ['order-1', 'order-2', 'order-3', 'order-4'];
